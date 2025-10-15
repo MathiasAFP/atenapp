7080385b8aa9ec7c127aa5337ce29f3c75070929
dallalaste <gabrieldallelaste75@gmail.com>
2025-10-15 14:41:14 -0300

    feat: signup controller e model com todo o código para funcionar

--- api/controllers/credentialController.js
+++ api/controllers/credentialController.js
@@ -1,45 +1,33 @@
-const credentialModel = require("../models/credentialModel");
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
+const credentialModel = require("../models/credentialModel");//arquivo que contem as funções do model
+const bcrypt = require('bcrypt');//biblioteca de criptografia
+const jwt = require('jsonwebtoken');//biblioteca para tokens
 require('dotenv').config();
 
-async function SignUpController(req, res) {
-    const {name, email, password, cod} = req.body;
+async function credentialControllerSignup(req, res) { //nome padronizado com nome do arquivo mais sua função
+    const {name, email, password, school_name, your_code} = req.body;//armazenar os valores da requisição
     
-    try{
-      const IsSchool = await credentialModel.IsSchool(cod)
-      const IsTeacher = await credentialModel.IsTeacher(name, cod)
+    try{//assincronismo
+      const whichSchool = await credentialModel.whichSchool(school_name);//verificar qual é a escola
+      const school_id = whichSchool.id;//salvar o id da escola
+      const teacher_code = whichSchool.teachercode;//salvar o código de professor da escola
+      const student_code = whichSchool.studentcode;//salvar o código de estudante da escola
 
-      if (IsSchool == 200) {
-        
+      if (teacher_code == your_code) {//verificar se é professor
+        await credentialModel.teacherSignup(name, email, password);
+        const teacher = await credentialModel.searchTeacher(name);
+        await credentialModel.doSchoolTeacherRelation(school_id, teacher.id);
       }
-      if (IsTeacher == 200) {
-        
+
+      if (student_code == your_code) {//verificar se é aluno
+        await credentialModel.studentSignup(name, email, password)
+        const student = await credentialModel.searchStudent(name);
+        await credentialModel.doSchoolStudentRelation(school_id, teacher.id);
       }
-        
     }
+
     catch{
         res.status(500).json({message: "UserSignUpControllerError"});
     }
-    
 };
 
-async function LoginController(req, res) {
-  const { name, email, password, cod } = req.body;
-  try {
-    const user = await credentialModel.UserLoginModel(name, email);
-    const match = await bcrypt.compare(password, user.password);
-    if (match) {
-      const token = jwt.sign({'name':name, 'email':email}, process.env.JWT_SECRET, {expiresIn:'1h'});
-
-      res.status(200).json({ token:token });
-    } else {
-      res.status(500).json({ message: "UserLoginControllerError" });
-    }
-  } catch (err) {
-    res.status(500).json({ message: "UserLoginControllerError" });
-  }
-}
-
-
-module.exports = {SignUpController, LoginController};
+module.exports = {credentialControllerSignup};
--- api/models/credentialModel.js
+++ api/models/credentialModel.js
@@ -1,53 +1,107 @@
 const db = require("../db");
 
-async function IsSchool(cod) {
+async function credentialModelWhichSchool(school_name) {
   return new Promise((resolve, reject) => {
-    const query = "SELECT * FROM Schools WHERE cod = ?";
-    db.query(query, [cod], (error, result) => {
+    const query = "SELECT * FROM school WHERE name = ?";
+    db.query(query, [school_name], (error, result) => {
       if (error) {
-        return reject(error);
+        return reject(500);
       }
       if (result.length > 0) {
         return resolve(result[0]);
       } else {
-        return reject("School was not find");
+        return reject(500);
       }
     });
   });
 }
 
-async function IsTeacher(name) {
+async function credentialModelTeacherSignup(name, email, password) {
   return new Promise((resolve, reject) => {
-    const query = "SELECT * FROM TeacherSchools WHERE name = ? AND cod = ?";
+    const query = "INSERT INTO teacher name = ? AND email = ? AND password = ?";
+    db.query(query, [name, email, password], (error, result) => {
+      if (error) {
+        return reject(500);
+      }
+      else {
+        return resolve(200);
+      }
+    });
+  });
+}
+
+async function credentialModelStudentSignup(name, email, password) {
+  return new Promise((resolve, reject) => {
+    const query = "INSERT INTO student name = ? AND email = ? AND password = ?";
+    db.query(query, [name, email, password], (error, result) => {
+      if (error) {
+        return reject(500);
+      }
+      else {
+        return resolve(200);
+      }
+    });
+  });
+}
+
+async function credentialModelSearchTeacher(name) {
+  return new Promise((resolve, reject) => {
+    const query = "SELECT * FROM teacher WHERE name = ?";
     db.query(query, [name], (error, result) => {
       if (error) {
-        return reject(error);
+        return reject(500);
       }
       if (result.length > 0) {
         return resolve(result[0]);
       } else {
-        return reject("Student");
+        return reject(500);
       }
     });
   });
 }
 
-async function SignUp(name, email, password) {
+async function credentialModelSearchStudent(name) {
   return new Promise((resolve, reject) => {
-    const query = "INSERT INTO users name = ?..."//esse comando esta errado;
+    const query = "SELECT * FROM student WHERE name = ?";
     db.query(query, [name], (error, result) => {
       if (error) {
-        return reject(error);
+        return reject(500);
       }
       if (result.length > 0) {
         return resolve(result[0]);
       } else {
-        return reject("Student");
+        return reject(500);
       }
     });
   });
 }
 
+async function credentialModelDoSchoolTeacherRelation(school_id, teacher_id) {
+  return new Promise((resolve, reject) => {
+    const query = "INSERT INTO schoolteacher school_id = ? AND teacher_id = ?";
+    db.query(query, [school_id, teacher_id], (error, result) => {
+      if (error) {
+        return reject(500);
+      }
+      else {
+        return resolve(200);
+      }
+    });
+  });
+}
 
+async function credentialModelDoSchoolStudentRelation(school_id, student_id) {
+  return new Promise((resolve, reject) => {
+    const query = "INSERT INTO schoolteacher school_id = ? AND teacher_id = ?";
+    db.query(query, [school_id, student_id], (error, result) => {
+      if (error) {
+        return reject(500);
+      }
+      else {
+        return resolve(200);
+      }
+    });
+  });
+}
 
-module.exports = {IsSchool, IsTeacher};
+module.exports = {credentialModelWhichSchool, credentialModelTeacherSignup, credentialModelStudentSignup, credentialModelSearchTeacher, credentialModelSearchStudent, credentialModelDoSchoolTeacherRelation, credentialModelDoSchoolStudentRelation};
